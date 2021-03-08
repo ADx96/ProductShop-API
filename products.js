f729466ea591d089e493d26d6a38130f1d59dfe9
ADx96 <aboodrous@gmail.com>
2021-03-08 20:55:58 +0300

    done till create

---
+++ products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "Ps5 Controller",
+    price: 15,
+    slug: "ps5-controller",
+    description: "a ps5 controller ",
+  },
+
+  {
+    id: 2,
+    name: "Gameing Headset",
+    price: 20,
+
+    slug: "gaming-headset",
+    description: "gaming headset for ps5/pc/xbox",
+  },
+  {
+    id: 3,
+    name: "LED Gaming Keyboard",
+    price: 7,
+
+    slug: "led-gaming-keyboard",
+    description: "Led Keyboard supports all consoles/pc ",
+  },
+];
+
+module.exports = products;
