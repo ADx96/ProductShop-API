f28855973a5989b508523104636c9690d52ae932
ADx96 <aboodrous@gmail.com>
2021-03-09 19:15:32 +0300

    done

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 let products = require("./products");
 const cors = require("cors");
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(cors());
 const slugify = require("slugify");
